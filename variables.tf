cb53c609b724a742aff99b05b028b539acf64545
ranganr <45820788+ranganr@users.noreply.github.com>
2025-12-31 12:17:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,40 @@
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "pipeline_name" {
+  type        = string
+  description = "CodePipeline name"
+  default     = "github-codepipeline"
+}
+
+variable "project_name" {
+  type        = string
+  description = "CodeBuild project name"
+  default     = "github-codebuild-project"
+}
+
+variable "artifact_bucket" {
+  type        = string
+  description = "S3 bucket for CodePipeline artifacts (must have versioning enabled)"
+}
+
+# GitHub (CodeStar Connections)
+variable "github_owner" {
+  type        = string
+  description = "GitHub organization or user"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub repository name"
+}
+
+variable "github_branch" {
+  type        = string
+  description = "Branch to build"
+  default     = "main"
+}
